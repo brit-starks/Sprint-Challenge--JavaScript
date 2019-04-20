67697cc2f52b2689218c05272872b8317f6bbaf4
brit-starks <brittneytechiethomas@gmail.com>
2019-04-19 20:27:44 -0700

    add callbacks to functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,34 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const consume = function(param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+// -------------------------------ADDITION CB-------------------------------------
+const add = function(num1, num2, cb) {
+  return cb(num1 + num2);
+}
+// console.log(add(2,3));
+//--------------------------------MULTIPLY CB-------------------------------------
+
+const multiply = function(num1, num2, cb) {
+  return cb(num1 * num2);
+}
+
+// console.log(multiply(3,4));
+
+//--------------------------------GREETING----------------------------------------
+
+function greeting(firstName, lastName, cb) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
