c47bb16b2ef6ea20b52720ba81779bd8030fcca3
brit-starks <brittneytechiethomas@gmail.com>
2019-04-20 20:43:31 -0700

    add classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,4 +4,37 @@
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
+class CuboidMaker {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+}
+
+const Cuboid = new CuboidMaker(4, 5, 5);
+console.log(Cuboid);
+
+  CuboidMaker.prototype.volume = function(){
+    return `${this.length} * ${this.width} * ${this.height}`;
+  }
+
+  CuboidMaker.prototype.surfaceArea = function(){
+    return `2 * ${this.length} * ${this.width} + ${this.length} *${this.height} + ${this.width} * ${this.height}`;
+  }
+  
+  
+  // function cuboid(attributes) {
+  //   CuboidMaker.call(this, attributes);
+  //   this.iscuboid = attributes.isCuboid;
+  // }
+  
+  // cuboid.prototype = Object.create(CuboidMaker.prototype);
+  
+  // const Cuboid = new cuboid({
+  //   length: 4,
+  //   width: 5,
+  //   height: 5
+  // });
+
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/functions.js
+++ challenges/functions.js
@@ -1,63 +1,63 @@
-// ==== Callbacks ====  
+// // ==== Callbacks ====  
 
-/* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
+// /* Step 1: Create a higher-order function that accepts a callback
+//   * Create a higher-order function named consume that can take 3 parameters.
+//   * The first two parameters can accept any argument
+//   * The last parameter accepts a callback 
+//   * In the body of the function return the callback with the two parameters that you created
+// */
 
-const consume = function(param1, param2, callback) {
-  callback(param1, param2);
-};
+// const consume = function(param1, param2, callback) {
+//   callback(param1, param2);
+// };
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
-// -------------------------------ADDITION CB-------------------------------------
-const add = function(param1, param2) {
-  console.log( param1 + param2);
-}
+// /* Step 2: Create several functions to callback with consume();
+//   * Create a function named add that returns the sum of two numbers
+//   * Create a function named multiply that returns the product of two numbers 
+//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+// */
+// // -------------------------------ADDITION CB-------------------------------------
+// const add = function(param1, param2) {
+//   console.log( param1 + param2);
+// }
 
-//--------------------------------MULTIPLY CB-------------------------------------
+// //--------------------------------MULTIPLY CB-------------------------------------
 
-const multiply = function(num1, num2) {
- console.log(num1 * num2);
-}
+// const multiply = function(num1, num2) {
+//  console.log(num1 * num2);
+// }
 
-//--------------------------------GREETING----------------------------------------
+// //--------------------------------GREETING----------------------------------------
 
-function greeting(firstName, lastName) {
-  console.log(`Hello ${firstName} ${lastName}, nice to meet you!`);
-}
-
-
-
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+// function greeting(firstName, lastName) {
+//   console.log(`Hello ${firstName} ${lastName}, nice to meet you!`);
+// }
 
 
-// ==== Closures ==== 
 
-// Explain in your own words why `nestedfunction()` can access the variable `internal`.
+// /* Step 3: Check your work by un-commenting the following calls to consume(): */
+// consume(2,2,add); // 4
+// consume(10,16,multiply); // 160
+// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
-// Explanation: 
 
-// nestedFunction is able to reach outside of its own scope (into global scope) to find the variable.
+// // ==== Closures ==== 
 
-const external = "I'm outside the function";
+// // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// // Explanation: 
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+// // nestedFunction is able to reach outside of its own scope (into global scope) to find the variable.
+
+// const external = "I'm outside the function";
+
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
+
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
